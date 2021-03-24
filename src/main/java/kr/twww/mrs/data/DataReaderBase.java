1e1d52a65b9949ed852c0800484f88cb32abf411
jinmyeongAN <jinmyeong.an@gmail.com>
2021-03-24 20:22:47 +0900

    DataReaderImpl 파일 TODO 작성

--- src/main/java/kr/twww/mrs/data/DataReaderBase.java
+++ src/main/java/kr/twww/mrs/data/DataReaderBase.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public abstract class DataReaderBase
 {
     public abstract String GetPathFromDataType( DataType dataType );
-    public abstract String ReadTextFromFile( String path );
+    public abstract ArrayList<String[]> ReadTextFromFile(String path );
 
     public abstract ArrayList<User> ToUserList( String text );
     public abstract ArrayList<Movie> ToMovieList( String text );
--- src/main/java/kr/twww/mrs/data/DataReaderImpl.java
+++ src/main/java/kr/twww/mrs/data/DataReaderImpl.java
@@ -1,13 +1,19 @@
 package kr.twww.mrs.data;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import static kr.twww.mrs.data.DataType.*;
 
 public class DataReaderImpl extends DataReaderBase implements DataReader
 {
     @Override
     public ArrayList<User> GetUserList()
     {
-        var path = GetPathFromDataType(DataType.USER);
+        var path = GetPathFromDataType(USER);
         var text = ReadTextFromFile(path);
         var result = ToUserList(text);
 
@@ -17,7 +23,7 @@ public class DataReaderImpl extends DataReaderBase implements DataReader
     @Override
     public ArrayList<Movie> GetMovieList()
     {
-        var path = GetPathFromDataType(DataType.MOVIE);
+        var path = GetPathFromDataType(MOVIE);
         var text = ReadTextFromFile(path);
         var result = ToMovieList(text);
 
@@ -27,7 +33,7 @@ public class DataReaderImpl extends DataReaderBase implements DataReader
     @Override
     public ArrayList<Rating> GetRatingList()
     {
-        var path = GetPathFromDataType(DataType.RATING);
+        var path = GetPathFromDataType(RATING);
         var text = ReadTextFromFile(path);
         var result = ToRatingList(text);
 
@@ -37,20 +43,64 @@ public class DataReaderImpl extends DataReaderBase implements DataReader
     @Override
     public String GetPathFromDataType( DataType dataType )
     {
+
+        if (dataType == USER)
+            return "../../../data/users.dat";
+        else if (dataType == MOVIE)
+            return "../../../data/movies.dat";
+        else if  (dataType == RATING)
+            return "../../../data/ratings.dat";
+        else
+            return "NO DATA";
+
         // TODO: 데이터 타입에 따른 데이터파일 경로 반환
-        return null;
     }
 
     @Override
-    public String ReadTextFromFile( String path )
-    {
+    public ArrayList<String[]> ReadTextFromFile(String path ) {
+        ArrayList<String[]> MyList = new ArrayList<>()
+        try {
+
+            BufferedReader br = new BufferedReader(new FileReader(path));
+
+            String line = null;
+
+            while((line = br.readLine()) != null) {
+                String str = line;
+                String[] ElementList = str.split("::");
+                MyList.add(ElementList);
+
+            }
+
+            br.close();
+
+        } catch (FileNotFoundException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+
+            e.printStackTrace();
+
+        }
+        //2차원 배열 출력
+        return (ArrayList<String[]>) MyList;
+
+
         // TODO: 주어진 경로의 파일을 읽어 String으로 반환
-        return null;
     }
 
     @Override
     public ArrayList<User> ToUserList( String text )
     {
+        for(String i : text ){
+            //i 는 users.dat 파일 한 줄씩임
+            //i의 순서는 userid, gender, age, occupation, zip-code 임
+            ConvertGender(i[1]);
+            ConvertAge(i[2]);
+            ConvertOccupation(i[3]);
+        }
         // TODO: 주어진 텍스트를 User 리스트로 반환
         return null;
     }
@@ -58,6 +108,12 @@ public class DataReaderImpl extends DataReaderBase implements DataReader
     @Override
     public ArrayList<Movie> ToMovieList( String text )
     {
+        for(String i : text ){
+            //i 는 movies.dat 파일 한 줄씩임
+            //i 의 세번째 값은 Genres 임
+            ConvertGenre(i[2]);
+        }
+
         // TODO: 주어진 텍스트를 Movie 리스트로 반환
         return null;
     }
