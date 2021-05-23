4573ce55195f120e2064b893044bdb23560fa5ef
BSCraker <bscraker@naver.com>
2021-05-24 00:05:11 +0900

    hotfix

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 git clone https://github.com/wbjeon2k/CSE364.git
 cd ./CSE364
 mvn package
-java -Xms1g -Xmx4g -jar target/cse364-project-1.0-SNAPSHOT-allinone.jar
+java -jar target/cse364-project-1.0-SNAPSHOT-allinone.jar
