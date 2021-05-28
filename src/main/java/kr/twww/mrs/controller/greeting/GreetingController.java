78232cd4c692947bf0628bb352a46ccf26c12733
wbjeon2k <wbjeon2k@users.noreply.github.com>
2021-05-29 00:59:02 +0900

    2021-05-28 mongodb jquery practice

--- src/main/java/kr/twww/mrs/controller/greeting/GreetingController.java
+++ src/main/java/kr/twww/mrs/controller/greeting/GreetingController.java
@@ -2,9 +2,11 @@ package kr.twww.mrs.controller.greeting;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class GreetingController {
@@ -12,6 +14,7 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
