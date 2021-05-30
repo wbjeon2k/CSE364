e2d4ef8065e4fde9e617cd36835d93d68b85c698
wbjeon2k <wbjeon2k@users.noreply.github.com>
2021-05-30 17:06:10 +0900

    2021-05-28 mongodb jquery practice

--- src/main/java/kr/twww/mrs/controller/greeting/hello.js
+++ src/main/java/kr/twww/mrs/controller/greeting/hello.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $.ajax({
-        url: "http://rest-service.guides.spring.io/greeting"
-    }).then(function(data) {
-        $('.greeting-id').append(data.id);
-        $('.greeting-content').append(data.content);
+        //url: "http://loalhost:8080/user/recommendations/"
+        url : "http://localhost:8080/users/recommendations?gender=M&age=&occupation=retired&genres=/"
+}).then(function(data) {
+        $('.greeting-id').append(data.title);
     });
 });
