a34b4a5263eb0778a1677328db0744541d41e69c
wbjeon2k <wbjeon2k@users.noreply.github.com>
2021-06-02 03:11:07 +0900

    2021-06-02 working on mongodb

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/wbjeon2k/CSE364.git
+git clone -b feature-jquery-practice https://github.com/wbjeon2k/CSE364.git
 cd ./CSE364
 mvn package
 java -jar target/cse364-project-1.0-SNAPSHOT-allinone.jar
