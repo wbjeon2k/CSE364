aead6b1a596c7f289c9305ac398fd680324b0f17
wbjeon2k <wbjeon2k@users.noreply.github.com>
2021-06-06 07:40:49 +0900

    2021-06-04 adding index control

--- mongdoinstall.sh
+++ mongdoinstall.sh
@@ -1,5 +1,6 @@
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+mkdir ../../data
+mkdir ../../data/db
 apt-get update
-mkdir "../../data/db"
 apt-get install -y mongodb-org
