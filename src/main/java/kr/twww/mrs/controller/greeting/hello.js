3e418cc70cd6a0457a380ec59c640202a5db5d8a
wbjeon2k <wbjeon2k@users.noreply.github.com>
2021-06-06 21:35:48 +0900

    2021-06-04 added comments

--- src/main/java/kr/twww/mrs/controller/greeting/hello.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $.ajax({
-        //url: "http://loalhost:8080/user/recommendations/"
-        url : "http://localhost:8080/users/recommendations?gender=M&age=&occupation=retired&genres=/"
-}).then(function(data) {
-        $('.greeting-id').append(data.title);
-    });
-});
--- src/main/resources/public/hello.js
+++ src/main/resources/public/hello.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $.ajax({
-        url: "http://rest-service.guides.spring.io/greeting"
+        url: "http://localhost:8080/greeting"
     }).then(function(data) {
         $('.greeting-id').append(data.id);
         $('.greeting-content').append(data.content);
