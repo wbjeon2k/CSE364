e29d83bcaa9bff03a3229fdaaa14c74ea2790110
wbjeon2k <wbjeon2k@users.noreply.github.com>
2021-06-08 01:07:49 +0900

    2021-06-04 run.sh 수정 완료

--- run.sh
+++ run.sh
@@ -7,14 +7,16 @@ rm -rf CSE364
 git clone -b feature-mongodb https://github.com/wbjeon2k/CSE364.git
 cd ./CSE364
 
-cp mongod.conf ../../../etc/mongod.conf
 cp mongod_install.sh ../mongod_install.sh
 
 cd ..
+pwd
 chmod a+x mongod_install.sh
 ./mongod_install.sh
-
+cp mongod.conf ../../../etc/mongod.conf
 #run mongod at background. logging on console is on.
 mongod &
+cd ./CSE364
+pwd
 mvn clean package
 java -jar target/cse364-project-1.0-SNAPSHOT-allinone.jar
